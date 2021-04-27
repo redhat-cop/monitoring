dd70ad65daf6aefe7b9a49085a24a6b2526a25fe
Jakub Filipczak <jfilipcz@redhat.com>
2021-04-27 15:09:58 +0200

    Getting rid of AWS CLI

--- exporters/aws-sq-exporter/README.md
+++ exporters/aws-sq-exporter/README.md
@@ -1,6 +1,6 @@
 ## AWS Service Quotas Exporter ##
 *** ***
-This is a simple Prometheus Exporter that querries AWS CLI for quota values of specific configuration items and calculates actual usage of those quotas. 
+This is a simple Prometheus Exporter that querries AWS API for quota values of specific configuration items and calculates actual usage of those quotas.
 
 ### AWS SQs ###
 *** ***
@@ -14,10 +14,17 @@ Docker image should be based on provided Dockerfile, build it by using standard 
     export VERSION="0.1.1"; docker build -t sq-exporter:${VERSION} ./
 
 ## Running the exporter and AWS credentials ##
-For the moment exporter uses AWS CLI, and relies on credential mechanism provided by AWS CLI itself. Simplest way of injecting API keys is by mounting prepopulated .aws into the container:
+Exporter uses AWS API directly, simplest way of injecting API keys is by mounting prepopulated .aws into the container:
 
     docker run  -p 8000:8000 -v /${HOME}/.aws:/home/exporter/.aws sq-exporter:0.1.1
 
+Other options are:
 
-
-
+-c AWSCONFIG, --awsconfig AWSCONFIG : Location of optional configuration file, if not
+                                      provided uses AWS CLI default
+-a APIKEY, --apikey APIKEY          : AWS Access Key ID
+-s SECRETKEY, --secretkey SECRETKEY : AWS Sercet Access Key
+-r REGION, --region REGION          : AWS Region to be used for queries
+-t TIME, --time TIME                : Sleep time between fetching the AWS API input
+-d, --debug                         : Should we be more verbose?
+-p PORT, --port PORT                : TCP port to be used to expose metrics HTTP endpoint
--- exporters/aws-sq-exporter/sq_exporter.py
+++ exporters/aws-sq-exporter/sq_exporter.py
@@ -6,48 +6,35 @@ from collections import namedtuple
 from prometheus_client import start_http_server, Summary, Gauge
 import argparse
 import time
-import yaml
-
-# Simple JSON mappers
-def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
-def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
+import boto3
 
 
-#Generic to fetch administrative quota value
-def getQuotaValue(quotaCode,serviceCode,envVars):
-   psq = subprocess.run(['/usr/local/bin/aws','service-quotas','list-service-quotas','--service-code',serviceCode ],stdout=subprocess.PIPE, universal_newlines=True, env=envVars)
-   if args.debug == True:
-     print(str(psq.args)+str(psq.stdout))
-   psq_j = json2obj(psq.stdout)
-   for i in psq_j.Quotas:
-     if i.QuotaCode == quotaCode:
-         currentQ = str(i.Value)
+#Generic function to fetch administrative quota value
+def getQuotaValue(quotaCode,serviceCode,cSessions):
+   paginator = cSessions["service-quotas"].get_paginator('list_service_quotas')
+   pCursor = paginator.paginate(ServiceCode=serviceCode, PaginationConfig={'MaxItems': 1000 , 'PageSize': 10})
+   i=0
+   currentQ = 0
+   for page in pCursor:
+      for page2 in page['Quotas']:
+         print(page2['QuotaCode'])
+         if page2['QuotaCode'] == quotaCode:
+          currentQ = str(page2['Value'])
+         i= i+1
+      print("Counter"+str(i))
+      print("Current Quota is "+str(currentQ))
    return currentQ
 
+
 # Calculate number of Elastic IP currently in use
-def getEipUsage(envVars):
-    
-    psu = subprocess.run(['/usr/local/bin/aws','ec2','describe-addresses'],stdout=subprocess.PIPE, universal_newlines=True, env=envVars)
-    psu_j = json2obj(psu.stdout)
-    if args.debug == True:
-      print(str(psu.args)+str(psu.stdout))
-    currentUsage = 0
-    for i in psu_j.Addresses:
-       currentUsage=currentUsage+1
-    return currentUsage
+def getEipUsage(cSessions):
+   awsReturn  = cSessions["ec2"].describe_addresses()
+   return len(awsReturn["Addresses"])
 
 # Extract usage for "VPCs per Region"
-def getVPCNumUsage(envVars):
-    psu = subprocess.run(['/usr/local/bin/aws','ec2','describe-vpcs'],stdout=subprocess.PIPE, universal_newlines=True, env=envVars)
-    psu_j = json2obj(psu.stdout)
-    if args.debug == True:
-      print(str(psu.args)+str(psu.stdout))
-    currentUsage = 0
-    for i in psu_j.Vpcs:
-      currentUsage=currentUsage+1
-    return currentUsage
-
-
+def getVPCNumUsage(cSessions):
+   awsReturn  = cSessions["ec2"].describe_vpcs()
+   return len(awsReturn["Vpcs"])
 
 
 
@@ -61,10 +48,27 @@ if __name__ == '__main__':
    parser.add_argument("-t","--time", type=int, default=1200 , help="Sleep time between fetching the AWS API input")
    parser.add_argument("-d","--debug", help=" Should we be more verbose?",action="store_true")
    parser.add_argument("-p","--port", default=8000, help=" TCP port to be used to expose metrics HTTP endpoint")
+   parser.add_argument("-m","--metrics", default="./metrics.yaml", help=" Metrics definition file")
    args = parser.parse_args()
 
+   ##Adding metrics.yaml parsing( we need to load up clients for all unique services)
+
+
+   awsCredentials = {}
+   awsCredentials["AWS_ACCESS_KEY_ID"]=args.apikey
+   awsCredentials["AWS_SECRET_ACCESS_KEY"]=args.secretkey
+   awsCredentials["AWS_DEFAULT_REGION"]=args.region
+   ##ec2 and VPC session set up
+   clientSessions= {}
+   clientSessions["ec2"] = boto3.client("ec2", aws_access_key_id=args.apikey, aws_secret_access_key= args.secretkey, region_name= args.region)
+   clientSessions["service-quotas"] = boto3.client("service-quotas", aws_access_key_id=args.apikey, aws_secret_access_key= args.secretkey, region_name= args.region)
+   clientSessions["ec2"] = boto3.client("ec2", aws_access_key_id=args.apikey, aws_secret_access_key= args.secretkey, region_name= args.region)
+   #clientSessions["vpc"] = boto3.client("vpc", aws_access_key_id=args.apikey, aws_secret_access_key= args.secretkey, region_name= args.region)
+   VPCs= clientSessions["ec2"].describe_addresses()
+   print(VPCs)
+
    # Create a metrics.
-  
+
    AwsEipQuota = Gauge('aws_eip_quota_value', 'Administrative Quota set on EIP')
    AwsEipUsage = Gauge('aws_eip_quota_usage', 'Number of Elastic IPs in use')
    AwsVPCNumQuota = Gauge('aws_vpc_per_region_quota_value', 'Administrative Quota set on "VPCs per Region"')
@@ -74,24 +78,16 @@ if __name__ == '__main__':
       print("TCP port:"+str(args.port))
       print("Interval:"+str(args.time))
       print("AWS CONFIG:"+str(args.awsconfig))
-   # set the AWS CLI env variables
-   mEnv = {}
-   if args.apikey and args.secretkey:
-      mEnv["AWS_ACCESS_KEY_ID"]=args.apikey
-      mEnv["AWS_SECRET_ACCESS_KEY"]=args.secretkey
-   if args.region: 
-      mEnv["AWS_DEFAULT_REGION"]=args.region
-   mEnv["AWS_CONFIG_FILE"]=args.awsconfig
 
    # Start HTTP Server
    start_http_server(args.port)
    print("Started AWS Service Quota Exporter listening on port:"+str(args.port))
    while True:
      #EIP Quota value
-     AwsEipQuota.set(getQuotaValue("L-0263D0A3","ec2",mEnv))
-     AwsEipUsage.set(getEipUsage(mEnv))
+     AwsEipQuota.set(getQuotaValue("L-0263D0A3","ec2",clientSessions))
+     AwsEipUsage.set(getEipUsage(clientSessions))
      #VPC Quota value
-     AwsVPCNumQuota.set(getQuotaValue("L-F678F1CE","vpc",mEnv))
-     AwsVPCNumUsage.set(getVPCNumUsage(mEnv))
+     AwsVPCNumQuota.set(getQuotaValue("L-F678F1CE","vpc",clientSessions))
+     AwsVPCNumUsage.set(getVPCNumUsage(clientSessions))
      time.sleep(args.time)
 exit()
