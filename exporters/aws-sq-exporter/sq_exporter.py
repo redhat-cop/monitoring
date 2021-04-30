454ce9ecadfaa52e952593afc394da0600eff857
Jakub Filipczak <jfilipcz@redhat.com>
2021-04-30 17:08:29 +0200

    Added separate metrics definition file

--- exporters/aws-sq-exporter/sq_exporter.py
+++ exporters/aws-sq-exporter/sq_exporter.py
@@ -7,7 +7,8 @@ from prometheus_client import start_http_server, Summary, Gauge
 import argparse
 import time
 import boto3
-
+import bios
+from benedict import benedict
 
 #Generic function to fetch administrative quota value
 def getQuotaValue(quotaCode,serviceCode,cSessions):
@@ -17,25 +18,17 @@ def getQuotaValue(quotaCode,serviceCode,cSessions):
    currentQ = 0
    for page in pCursor:
       for page2 in page['Quotas']:
-         print(page2['QuotaCode'])
          if page2['QuotaCode'] == quotaCode:
           currentQ = str(page2['Value'])
          i= i+1
-      print("Counter"+str(i))
-      print("Current Quota is "+str(currentQ))
    return currentQ
 
 
 # Calculate number of Elastic IP currently in use
-def getEipUsage(cSessions):
-   awsReturn  = cSessions["ec2"].describe_addresses()
-   return len(awsReturn["Addresses"])
-
-# Extract usage for "VPCs per Region"
-def getVPCNumUsage(cSessions):
-   awsReturn  = cSessions["ec2"].describe_vpcs()
-   return len(awsReturn["Vpcs"])
-
+def getUsage(cSessions,usageRetrieval,usageFilter):
+   awsCall = getattr(cSessions["ec2"],usageRetrieval)
+   awsReturn = awsCall()
+   return len(awsReturn[usageFilter])
 
 
 if __name__ == '__main__':
@@ -53,26 +46,25 @@ if __name__ == '__main__':
 
    ##Adding metrics.yaml parsing( we need to load up clients for all unique services)
 
+   metrics = benedict(args.metrics, format='yaml')
 
-   awsCredentials = {}
-   awsCredentials["AWS_ACCESS_KEY_ID"]=args.apikey
-   awsCredentials["AWS_SECRET_ACCESS_KEY"]=args.secretkey
-   awsCredentials["AWS_DEFAULT_REGION"]=args.region
+   if args.debug == True:
+       print("Loading metrics definition file located at "+str(args.metrics))
+
+   ## Get the services we'll need to connect to
+
    ##ec2 and VPC session set up
    clientSessions= {}
    clientSessions["ec2"] = boto3.client("ec2", aws_access_key_id=args.apikey, aws_secret_access_key= args.secretkey, region_name= args.region)
    clientSessions["service-quotas"] = boto3.client("service-quotas", aws_access_key_id=args.apikey, aws_secret_access_key= args.secretkey, region_name= args.region)
-   clientSessions["ec2"] = boto3.client("ec2", aws_access_key_id=args.apikey, aws_secret_access_key= args.secretkey, region_name= args.region)
-   #clientSessions["vpc"] = boto3.client("vpc", aws_access_key_id=args.apikey, aws_secret_access_key= args.secretkey, region_name= args.region)
-   VPCs= clientSessions["ec2"].describe_addresses()
-   print(VPCs)
 
-   # Create a metrics.
+   # Create the actual metrics.
+   for i in metrics["values"]:
+    if args.debug == True:
+      print("Creating metric for "+i['quotaCode']+" quota code")
+    i['mObjectUsage']= Gauge(i['metricNameUsage'], i['usageDesc'])
+    i['mObjectQuota']= Gauge(i['metricNameQuota'], i['quotaDesc'])
 
-   AwsEipQuota = Gauge('aws_eip_quota_value', 'Administrative Quota set on EIP')
-   AwsEipUsage = Gauge('aws_eip_quota_usage', 'Number of Elastic IPs in use')
-   AwsVPCNumQuota = Gauge('aws_vpc_per_region_quota_value', 'Administrative Quota set on "VPCs per Region"')
-   AwsVPCNumUsage = Gauge('aws_vpc_per_region_quota_usage', 'Number of VPCs  in use')
    if args.debug == True:
       print("Starting AWS Service Quota Exporter with the following parameters:")
       print("TCP port:"+str(args.port))
@@ -83,11 +75,8 @@ if __name__ == '__main__':
    start_http_server(args.port)
    print("Started AWS Service Quota Exporter listening on port:"+str(args.port))
    while True:
-     #EIP Quota value
-     AwsEipQuota.set(getQuotaValue("L-0263D0A3","ec2",clientSessions))
-     AwsEipUsage.set(getEipUsage(clientSessions))
-     #VPC Quota value
-     AwsVPCNumQuota.set(getQuotaValue("L-F678F1CE","vpc",clientSessions))
-     AwsVPCNumUsage.set(getVPCNumUsage(clientSessions))
+     for i in metrics["values"]:
+         i['mObjectQuota'].set(getQuotaValue(i['quotaCode'],i['serviceCode'],clientSessions))
+         i['mObjectUsage'].set(getUsage(clientSessions,i['usageRetrieval'],i['usageFilter']))
      time.sleep(args.time)
 exit()
